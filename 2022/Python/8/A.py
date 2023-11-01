08ce047dfa6fab2bfab4627c92a343c727733f1d
tdk93 <akshay.tdk93@gmail.com>
2023-10-31 19:25:16 -0700

    Remove swap file

--- 2022/Python/8/A.py
+++ 2022/Python/8/A.py
@@ -1,17 +1,16 @@
-import numpy as np
 
-def solve:
+def solve():
 
     with open('input.txt','r') as file:
         lines = file.readlines()
-        lines = [entry.strip() for entry in lines]
 
-    trees = np.zeros((len(lines), len(lines[0])), dtype=int)
-    for i,line in enumerate(lines):
-        trees[i,:] = np.array(list(line))
+def read_lines():
+    with open('input.txt','r') as file:
+        for line in file:
+            print("hello")
+            print(line)
+def main():
+    read_lines()
 
-
-    visible_trees = 2*len(lines[0]) + 2*(len(lines)-2)
-
-    for i in range(1,trees.shape[0]-1):
-        for j in range(1,trees.shape[1]-1):
+if __name__=="__main__":
+    main()

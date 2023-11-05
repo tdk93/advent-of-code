2e703f4531d3f5092f18c244aeecc95153e8ea8e
tdk93 <akshay.tdk93@gmail.com>
2023-11-05 12:51:22 -0800

    Added logic to calculate the correct score for a turn

--- 2022/Python/2/2_A.py
+++ 2022/Python/2/2_A.py
@@ -12,54 +12,55 @@ class Player(Enum):
 
 class TakeTurn:
 
-    def __init__(self, opp_move, my_move):
+    def __init__(self, my_move, opp_move):
+        self.my_move = my_move 
         self.opp_move = opp_move
-        self.my_move = my_move
 
-    def get_winner():
+    def get_winner(self):
         if self.opp_move == self.my_move:
-            return Player.NIL()
-        elif self.my_move ==  Move.ROCK() && self.opp_move == Move.SCISSORS():
-            RETURN Player.ME()
-        elif self.my_move ==  Move.SCISSOR() && self.opp_move == Move.PAPER():
-            return Player.ME()
-        elif self.my_move ==  Move.PAPER() && self.opp_move == Move.SCISSOR():
-            return Player.ME() 
+            return Player.NIL
+        elif self.my_move ==  Move.ROCK and self.opp_move == Move.SCISSOR:
+            return Player.ME
+        elif self.my_move ==  Move.SCISSOR and self.opp_move == Move.PAPER:
+            return Player.ME
+        elif self.my_move ==  Move.PAPER and self.opp_move == Move.SCISSOR:
+            return Player.ME 
         else:
-            return Player.OPPONENT()
+            return Player.OPPONENT
 
 
     def get_score(self)-> (int,int) :
         my_score = 0
         opponent_score = 0
 
-        if get_winner() == Player.ME():
+        if self.get_winner() == Player.ME:
             my_score += 6 
 
-        elif get_winner() == Player.OPPONENT():
+        elif self.get_winner() == Player.OPPONENT:
             opponent_score += 6 
         else:
             my_score += 3 
             opponent_score += 3  
 
-        if self.my_move == Move.ROCK():
+        if self.my_move == Move.ROCK:
             my_score += 1 
-        elif self.my_move == Move.PAPER():
+        elif self.my_move == Move.PAPER:
             my_score += 2 
         else:
             my_score += 3
 
-        if self.opp_move == Move.ROCK():
-            opp_score += 1 
-        elif self.opp_move == Move.PAPER():
-            opp_score += 2 
+        if self.opp_move == Move.ROCK:
+            opponent_score += 1 
+        elif self.opp_move == Move.PAPER:
+            opponent_score += 2 
         else:
-            opp_score += 3
+            opponent_score += 3
 
         return (my_score, opponent_score) 
 
 def main():
-    print("hello world")
+    tt = TakeTurn(Move.ROCK, Move.PAPER)
+    print((tt.get_score()))
 
 if __name__=="__main__":
     main()
